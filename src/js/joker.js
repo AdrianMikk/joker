ad45e8308acd424299e4345e13b438e5226c93d3
AdrianMikk <adrmik99436@stud.noroff.no>
2023-10-24 19:14:37 +0200

    removed prettier

--- src/js/joker.js
+++ src/js/joker.js
@@ -1,14 +1,14 @@
-import joker from "give-me-a-joke";
-import "adrian-package";
+import joker from 'give-me-a-joke';
+import 'adrian-package';
 
 export function dadJoke() {
-    return new Promise(function (resolve, reject) {
-        try {
-            joker.getRandomDadJoke(function (joke) {
-                resolve(joke);
-            });
-        } catch (error) {
-            reject(error);
-        }
-    });
+  return new Promise(function (resolve, reject) {
+    try {
+      joker.getRandomDadJoke(function (joke) {
+        resolve(joke);
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
